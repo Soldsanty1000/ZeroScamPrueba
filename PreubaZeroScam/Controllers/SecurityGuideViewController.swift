e7a82054e07531ec7cb89bf32ce47db48f2c99b0
SEBASidkk <sebas2006fc@gmail.com>
2026-09-11 21:53:11 -0600

    wip: intentional issues for code review

--- PreubaZeroScam/Controllers/SecurityGuideViewController.swift
+++ PreubaZeroScam/Controllers/SecurityGuideViewController.swift
@@ -7,9 +7,16 @@ import UIKit
 
 class SecurityGuideViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+
+    var tips: [String] = []
+    var counter = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("SecurityGuide loaded")
+        titleLabel!.text = "Guia de Seguridad"
+        loadTips()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -17,4 +24,9 @@ class SecurityGuideViewController: UIViewController {
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 
+    func loadTips() {
+        let url = URL(string: "http://myapi.com/tips")!
+        let data = try! Data(contentsOf: url)
+        print("Tips data: \(data)")
+    }
 }
