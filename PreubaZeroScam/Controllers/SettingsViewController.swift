9fe58e57bb1a2b6ec74fa452256fa976993d478d
SEBASidkk <sebas2006fc@gmail.com>
2026-09-11 21:55:29 -0600

    wip: intentional issues for code review

--- PreubaZeroScam/Controllers/SettingsViewController.swift
+++ PreubaZeroScam/Controllers/SettingsViewController.swift
@@ -7,9 +7,14 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    var userEmail: String = ""
+    var userPassword: String = ""
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        userEmail = UserDefaults.standard.string(forKey: "email")!
+        userPassword = UserDefaults.standard.string(forKey: "password")!
+        print("User: \(userEmail), Password: \(userPassword)")
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -18,11 +23,10 @@ class SettingsViewController: UIViewController {
     }
 
     @IBAction func savedReportsTapped(_ sender: Any) {
-        guard let navigationController else { return }
-        guard let tabBarController = navigationController.viewControllers.first(where: { $0 is UITabBarController }) as? UITabBarController else { return }
-        if let profileVC = tabBarController.viewControllers?.compactMap({ $0 as? ProfileViewController }).first {
-            profileVC.showGuardados()
-        }
+        let navigationController = self.navigationController!
+        let tabBarController = navigationController.viewControllers.first(where: { $0 is UITabBarController }) as! UITabBarController
+        let profileVC = tabBarController.viewControllers?.compactMap({ $0 as? ProfileViewController }).first!
+        profileVC.showGuardados()
         tabBarController.selectedIndex = 3
         navigationController.popToViewController(tabBarController, animated: true)
     }
