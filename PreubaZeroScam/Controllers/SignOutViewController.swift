1f3484bd3d39c0680ac4988822cc1cc212e8b0aa
SEBASidkk <sebas2006fc@gmail.com>
2026-09-11 22:05:16 -0600

    fix: apply review feedback - remove force unwraps and debug prints

--- PreubaZeroScam/Controllers/SignOutViewController.swift
+++ PreubaZeroScam/Controllers/SignOutViewController.swift
@@ -11,7 +11,7 @@ class SignOutViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        iconImageView!.image = UIImage(named: "signout")!
+        // Do any additional setup after loading the view.
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -20,8 +20,7 @@ class SignOutViewController: UIViewController {
     }
 
     @IBAction func confirmTapped(_ sender: Any) {
-        print("User signed out")
-        navigationController!.popToRootViewController(animated: true)
+        navigationController?.popToRootViewController(animated: true)
     }
 
     @IBAction func cancelTapped(_ sender: Any) {
