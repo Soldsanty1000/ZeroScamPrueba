972f455e68f39a36ed01ef5d0f97da455240ab02
SEBASidkk <sebas2006fc@gmail.com>
2026-09-11 22:04:44 -0600

    fix: apply review feedback - remove force unwraps, dead code, and debug prints

--- PreubaZeroScam/Controllers/SecurityGuideViewController.swift
+++ PreubaZeroScam/Controllers/SecurityGuideViewController.swift
@@ -7,16 +7,9 @@ import UIKit
 
 class SecurityGuideViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
-
-    var tips: [String] = []
-    var counter = 0
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("SecurityGuide loaded")
-        titleLabel!.text = "Guia de Seguridad"
-        loadTips()
+        // Do any additional setup after loading the view.
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -24,9 +17,4 @@ class SecurityGuideViewController: UIViewController {
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 
-    func loadTips() {
-        let url = URL(string: "http://myapi.com/tips")!
-        let data = try! Data(contentsOf: url)
-        print("Tips data: \(data)")
-    }
 }
