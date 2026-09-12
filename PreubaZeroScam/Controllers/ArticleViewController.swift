05abbbe82e99330d3839e3ddf6d3b96d04bbc71d
santi <bhlpsan@gmail.com>
2026-09-11 22:34:36 -0600

    feat: add ArticleViewController and root gitignore

---
+++ PreubaZeroScam/Controllers/ArticleViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ArticleViewController.swift
+//  PreubaZeroScam
+//
+//  Created by alumnos on 11/09/26.
+//
+
+
+import UIKit
+
+class ArticleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: xxx) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(xxx, animated: animated)
+    }
+}
