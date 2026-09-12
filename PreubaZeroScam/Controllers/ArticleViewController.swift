2c30a6630f887e9dbb4bd7501ce0921eb767be62
santi <bhlpsan@gmail.com>
2026-09-11 22:59:37 -0600

    fix: replace xxx placeholders with Bool type and false value

--- PreubaZeroScam/Controllers/ArticleViewController.swift
+++ PreubaZeroScam/Controllers/ArticleViewController.swift
@@ -15,8 +15,8 @@ class ArticleViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func viewWillAppear(_ animated: xxx) {
+    override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(xxx, animated: animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 }
