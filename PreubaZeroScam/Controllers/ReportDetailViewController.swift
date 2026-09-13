3083b55b747bb0fa9238be2c4d25286296484928
Alejandro R. <A01667608@tec.mx>
2026-09-13 12:27:01 -0600

    wip: intentional issues for code review

---
+++ PreubaZeroScam/Controllers/ReportDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ReportDetailViewController.swift
+//  PrototipoApp
+//
+
+import UIKit
+
+class ReportDetailViewController: UIViewController {
+
+    @IBOutlet weak var commentField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        commentField.applyOfraudeStyle()
+        hideKeyboardOnTap()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(xxx, animated: animated)
+    }
+
+    @IBAction func meTooTapped(_ sender: Any) {
+
+    }
+
+    @IBAction func saveTapped(_ sender: Any) {
+        // TODO: agregar a Reportes Guardados
+    }
+
+    @IBAction func shareTapped(_ sender: Any) {
+        // TODO: compartir reporte
+    }
+
+    @IBAction func postCommentTapped(_ sender: Any) {
+        // TODO: publicar comentario
+    }
+}
