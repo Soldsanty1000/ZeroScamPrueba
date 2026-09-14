e0ecb6f2fe6ad26f21df6285d02e386ed51eff62
Jose-Emilio1210 <a01669452@tec.mx>
2026-09-13 19:41:59 -0600

    Add HomeViewController IBOutlets and base setup

---
+++ PreubaZeroScam/Controllers/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  PreubaZeroScam
+//
+//  Created by José López Flores on 13/09/26.
+//
+
+import SwiftUI
+
+struct HomeViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeViewController()
+}
