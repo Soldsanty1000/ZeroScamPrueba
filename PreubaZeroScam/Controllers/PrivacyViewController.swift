bf1e9c8962a12ed8f53c1963bccca8d70c373e8e
Jose-Emilio1210 <a01669452@tec.mx>
2026-09-13 20:25:15 -0600

    Implement viewDidLoad lifecycle method

--- PreubaZeroScam/Controllers/PrivacyViewController.swift
+++ PreubaZeroScam/Controllers/PrivacyViewController.swift
@@ -7,4 +7,9 @@ import UIKit
 
 class PrivacyViewController: UIViewController {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
 }
