c9a1f04b6031fa67b02a4f93bea27866841c16d7
Jose-Emilio1210 <a01669452@tec.mx>
2026-09-13 23:29:18 -0600

    Add RegisterViewController IBOutlets and base setup

---
+++ PreubaZeroScam/Controllers/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  PreubaZeroScam
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var confirmPasswordField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
